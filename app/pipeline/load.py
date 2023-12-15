8de0c47225347c210c682475cd237fbb9596d171
VINICIUS FIALHO <vinicius.fialho06@gmail.com>
2023-12-15 09:03:39 -0300

    feat: formatando o meu código

--- app/pipeline/load.py
+++ app/pipeline/load.py
@@ -3,7 +3,9 @@ import os  # biblioteca para manipular arquivos e pastas
 import pandas as pd
 
 
-def load_excel(data_frame: pd.DataFrame, output_path: str, file_name: str) -> str:
+def load_excel(
+    data_frame: pd.DataFrame, output_path: str, file_name: str
+) -> str:
     """
     receber um dataframe e salvar como excel
 
@@ -18,5 +20,5 @@ def load_excel(data_frame: pd.DataFrame, output_path: str, file_name: str) -> st
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
-    data_frame.to_excel(f"{output_path}/{file_name}.xlsx", index=False)
-    return "Arquivo salvo com sucesso"
+    data_frame.to_excel(f'{output_path}/{file_name}.xlsx', index=False)
+    return 'Arquivo salvo com sucesso'
